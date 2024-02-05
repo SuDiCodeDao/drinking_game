0123a3c02da80bc7117e51ebd067719df33941f9
SuDiCodeDao <sunguyen.dev@gmail.com>
2024-02-05 09:47:31 +0700

    design kho quaaa..

--- lib/features/game_play/ui/screens/game_play_screen.dart
+++ lib/features/game_play/ui/screens/game_play_screen.dart
@@ -14,16 +14,19 @@ class GamePlayScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {
-              context.go(RouteName.gameModeSelection);
-            },
-            icon: const Icon(
-              Icons.arrow_back_ios_outlined,
-              size: 18,
-            )),
+          onPressed: () {
+            context.go(RouteName.gameModeSelection);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_outlined,
+            size: 18,
+          ),
+        ),
         actions: [
           IconButton(
-              onPressed: () {}, icon: const Icon(Icons.help_outline, size: 18))
+            onPressed: () {},
+            icon: const Icon(Icons.help_outline, size: 18),
+          ),
         ],
       ),
       body: SafeArea(
@@ -34,24 +37,40 @@ class GamePlayScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Align(
-                  alignment: Alignment.centerLeft,
-                  child: IconButton(
-                      style: IconButton.styleFrom(backgroundColor: Colors.grey),
-                      onPressed: () {},
-                      icon: const Icon(Icons.volume_up, size: 18))),
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  style: IconButton.styleFrom(backgroundColor: Colors.grey),
+                  onPressed: () {},
+                  icon: const Icon(Icons.volume_up, size: 18),
+                ),
+              ),
               Expanded(
-                child: Stack(
-                    alignment: Alignment.center,
+                child: Center(
+                  child: Stack(
                     children: List.generate(
-                        10,
-                        (index) => Positioned(
-                            bottom: index * 5.0,
-                            child: const CardItem(
-                              name: 'Haha',
-                            )))),
+                      9,
+                      (index) => Positioned(
+                          bottom: index * 5,
+                          left: (10 - index - 1) * 5,
+                          top: (10 - index - 1) * 5,
+                          right: index * 5,
+                          child: Card(
+                            child: Container(),
+                          )),
+                    )..add(
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 9 * 5, right: 9 * 5),
+                          child: CardItem(name: 'hihi'),
+                        ),
+                      ),
+                  ),
+                ),
               ),
               const SizedBox(height: 20.0),
-              ElevatedButton(onPressed: () {}, child: const Text('Kế tiếp'))
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Kế tiếp'),
+              ),
             ],
           ),
         ),
